8efa396d73e4e5722d62b4a049b6d1ab2bac5540
kah3vich <nik.b20@mail.ru>
2022-05-25 22:49:33 +0300

    new: install prettier

--- public/js/app.js
+++ public/js/app.js
@@ -2,8 +2,8 @@
 
 $(function () {
   var count = 1;
-  var rotateLogoTab = setInterval(function () {
-    $("#rotateIcon").attr("href", "img/icons-".concat(count, ".png"));
+  setInterval(function () {
+    $('#rotateIcon').attr('href', "img/icons-".concat(count, ".png"));
     count++;
 
     if (count === 24) {
@@ -12,8 +12,7 @@ $(function () {
   }, 250);
 });
 var rotateDeg = 360;
-$("#active").on("click", function () {
-  $("#gulp").css("transform", "translate(-50%, -50%) rotate(".concat(rotateDeg, "deg)"));
+$('#active').on('click', function () {
+  $('#gulp').css('transform', "translate(-50%, -50%) rotate(".concat(rotateDeg, "deg)"));
   rotateDeg += 360;
 });
-;
--- src/js/index/index.js
+++ src/js/index/index.js
@@ -1,16 +1,16 @@
 $(() => {
-	let count = 1;
-	const rotateLogoTab = setInterval(() => {
-		$("#rotateIcon").attr("href", `img/icons-${count}.png`);
-		count++;
+	let count = 1
+	setInterval(() => {
+		$('#rotateIcon').attr('href', `img/icons-${count}.png`)
+		count++
 		if (count === 24) {
-			count = 1;
+			count = 1
 		}
-	}, 250);
-});
+	}, 250)
+})
 
-let rotateDeg = 360;
-$("#active").on("click", () => {
-	$("#gulp").css("transform", `translate(-50%, -50%) rotate(${rotateDeg}deg)`);
-	rotateDeg += 360;
-});
+let rotateDeg = 360
+$('#active').on('click', () => {
+	$('#gulp').css('transform', `translate(-50%, -50%) rotate(${rotateDeg}deg)`)
+	rotateDeg += 360
+})
