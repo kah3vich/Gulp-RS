82d283435dd25453c5c49b13ba74919a93fc93fc
kah3vich <nik.b20@mail.ru>
2023-04-07 23:46:30 +0300

    test: flow read script

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 'use strict';
 
-module.exports = function buttonCustom(padding = '10px', color = 'red') {
+function buttonCustom(padding = '10px', color = 'red') {
 	const button = document.getElementById('custom');
 	button.style.padding = padding;
 	button.style.backgroundColor = color;
-};
+}
+
+module.exports = buttonCustom;
